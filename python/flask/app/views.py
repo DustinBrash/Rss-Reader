9e150cb88bb71193c341f0f2327f6ca1f1ff160c
Dustin Brashear <xrunner23@gmail.com>
2015-08-17 22:48:58 -0400

    Disolved feed package and merged with app. Have the base framwork of comic page in place. CSS sheet is good on formatting, but needs some jazzing up.

--- python/flask/app/views.py
+++ python/flask/app/views.py
@@ -1,6 +1,13 @@
 from app import app
+from flask import render_template
+#import feed
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello World!"
+"""Make sure that crawler starts from last link and
+	 works to first new in case of batch updates """
+
+@app.route('/comic')
+def comic():
+	#title, description, link
+	entries = [{"title": "My Comic", "description" : "A Comic I Made Up Just Now", "link" : "httP://www.google.com"}]
+	entries = entries * 5 
+	return render_template("comics.html", entries=entries)
