f91420ad47aacc4c5ddf35c94f0ca2b021eb3960
gitmanager <gitmanager@gitrepo.student.umd.edu>
2016-03-01 01:52:11 -0500

    Cleaning up testing

--- python/flask/app/feed/Entry.py
+++ python/flask/app/feed/Entry.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 class Entry():
 
     def __init__(self, item):
-        self.title= item.title.string
+        self.title= item.title.string if item.title is not None else item.link.string
         #below might lead to issues depending on how tags are parsed
         temp_link = item.link.string
         self.link = temp_link if temp_link is not None else item.link['href']
--- python/flask/run.py
+++ python/flask/run.py
@@ -1,3 +1,3 @@
-#!/home/pi/webvenv/bin/python
+#!/home/gitmanager/.virtualenvs/rss-feed/bin/python
 from app import app
 app.run(host='0.0.0.0', debug = False)
