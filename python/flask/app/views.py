f61fcef90759982a9d7c13f29499d6e98fac77e0
gitmanager <gitmanager@gitrepo.student.umd.edu>
2016-03-01 01:35:04 -0500

    Broke sites into separated text file for cleanliness and added Outcast comic to feed

--- python/flask/app/views.py
+++ python/flask/app/views.py
@@ -9,20 +9,8 @@ from feed.Feed import Feed
 @app.route('/comic')
 def comic():
     #title, description, link
-    url_list = [ "http://www.questionablecontent.net/QCRSS.xml" \
-		, "http://dailyderp.tumblr.com/rss" \
-		, "http://www.nerdragecomic.com/feed.xml" \
-		, "http://chaoslife.findchaos.com/feed" \
-		, "http://lilmissrarity.com/rss" \
-		, "http://studentofthenight.tumblr.com/rss" \
-		, "http://www.alicegrove.com/rss" \
-		, "http://feeds.feedburner.com/rsspect/fJur?format=xml" \
-		, "http://www.smbc-comics.com/rss.php" \
-		, "http://www.giantitp.com/comics/oots.rss" \
-		, "http://xkcd.com/rss.xml" \
-		, "http://twokinds.keenspot.com/rss.php" \
-		, "http://feeds.feedburner.com/AvasDemon?format=xml"]
-                    
-    feed_list = [Feed(url) for url in url_list] 
-    return render_template("comics.html", title = "Comics", feeds = feed_list)
+    with open("sites.txt") as f:
+    	url_list = f.readlines()
+   	feed_list = [Feed(url) for url in url_list] 
+    	return render_template("comics.html", title = "Comics", feeds = feed_list)
 
